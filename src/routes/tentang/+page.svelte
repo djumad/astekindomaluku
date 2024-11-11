73d9ec0c3e2bc2beb73e26184e156d48269c8c99
djumad <42491296+djumad@users.noreply.github.com>
2024-11-11 10:42:17 +0900

    Create +page.svelte

---
+++ src/routes/tentang/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let data;
+    let safeContent = '';
+    const sertifikasi = data.tentang;
+
+    onMount(() => {
+        safeContent = sanitizeHtml(data.sertifikasi.deskripsi_sertifikasi);
+    });
+
+    function sanitizeHtml(html) {
+        const div = document.createElement('div');
+        div.innerHTML = html;
+        return div.innerHTML;
+    }
+</script>
+
+<div class="max-w-4xl mx-auto my-8 p-6 bg-white shadow-lg rounded-lg border border-gray-200">
+    <p class="text-5xl font-bold text-center text-black mb-4">{data.sertifikasi.isi}</p>
+    <div class="prose prose-lg mx-auto">
+        <img src="https://astekindomaluku.pockethost.io/api/files/{sertifikasi.collectionId}/{sertifikasi.id}/{sertifikasi.foto}" alt="" width="350">
+        {@html safeContent}
+    </div>
+</div>
