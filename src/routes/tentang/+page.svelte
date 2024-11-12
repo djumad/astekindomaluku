e2c1baae2d8013d5479c56c1297847cbbacdfa10
djumad <42491296+djumad@users.noreply.github.com>
2024-11-12 16:22:29 +0900

    Update +page.svelte

--- src/routes/tentang/+page.svelte
+++ src/routes/tentang/+page.svelte
@@ -3,10 +3,10 @@
 
     export let data;
     let safeContent = '';
-    const sertifikasi = data.sertifikasi;
+    const tentang = data.tentang;
 
     onMount(() => {
-        safeContent = sanitizeHtml(data.sertifikasi.isi);
+        safeContent = sanitizeHtml(data.tentang.isi);
     });
 
     function sanitizeHtml(html) {
@@ -19,7 +19,6 @@
 <div class="max-w-4xl mx-auto my-8 p-6 bg-white shadow-lg rounded-lg border border-gray-200">
     <p class="text-5xl font-bold text-center text-black mb-4">Tentang Astekindo</p>
     <div class="prose prose-lg mx-auto">
-        <img src="https://astekindomaluku.pockethost.io/api/files/{sertifikasi.collectionId}/{sertifikasi.id}/{sertifikasi.foto_sertifikasi}" alt="" width="350">
         {@html safeContent}
     </div>
 </div>
