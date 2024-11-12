21392052aa298ee23cdcc828e0ea2230b20b2fd6
djumad <42491296+djumad@users.noreply.github.com>
2024-11-12 16:24:00 +0900

    Update +page.server.ts

--- src/routes/tentang/+page.server.ts
+++ src/routes/tentang/+page.server.ts
@@ -2,7 +2,7 @@ import pb from "$lib/pb";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-    const tentang = await pb.collection('tentang_astekindo').getOne('RECORD_ID');
+    const tentang = await pb.collection('tentang_astekindo').getOne('mkaeq77apcc89zx');
     return{
         tentang
     }
