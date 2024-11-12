97ff827debd69f566b7e59b7623b4ea8378ac9c6
djumad <42491296+djumad@users.noreply.github.com>
2024-11-12 16:17:16 +0900

    Update +page.server.ts

--- src/routes/tentang/+page.server.ts
+++ src/routes/tentang/+page.server.ts
@@ -2,9 +2,7 @@ import pb from "$lib/pb";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-    const tentang = await pb.collection('tentang_astekindo').getFullList({
-    sort: '-created',
-});
+    const tentang = await pb.collection('tentang_astekindo').getOne('RECORD_ID');
     return{
         tentang
     }
