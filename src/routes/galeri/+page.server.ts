5ced247753dac1059a0864f577996bc306b17974
djumad <42491296+djumad@users.noreply.github.com>
2024-11-14 03:58:34 +0900

    Update +page.server.ts

--- src/routes/galeri/+page.server.ts
+++ src/routes/galeri/+page.server.ts
@@ -5,11 +5,8 @@ export const load = (async () => {
     const sertifikasi = await pb.collection('sertifikasi').getList(1 , 50 , {
         expand : 'users_id'
     });
-    const tim = await pb.collection('tim_astekindo').getList(1 , 50 , {
-        expand : 'users_id'
-    })
     console.log(sertifikasi);
     return {
-        sertifikasi, tim, 
+        sertifikasi, 
     }
 }) satisfies PageServerLoad;
