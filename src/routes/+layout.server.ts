60a896971d9a748f8ee841438321bcb52510bf89
djumad <42491296+djumad@users.noreply.github.com>
2024-11-13 23:45:46 +0900

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,8 +2,7 @@ import pb from '$lib/pb';
 import type { LayoutServerLoad } from './$types';
 
 export const load = (async () => {
-    const status = await pb.collection('status').getFullList();
     const visiMisi = await pb.collection('visi_misi').getFullList();
 
-    return {status , visiMisi};
+    return {visiMisi};
 }) satisfies LayoutServerLoad;
